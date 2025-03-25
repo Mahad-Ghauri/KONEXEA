aa7fb46d5ed6ada2dfebe9476dd0d58c30f8f423
Mahad-Ghauri <mahadghauri222@gmail.com>
2025-03-25 23:39:21 +0500

    Refactor divider and logo components: comment out imports and class definitions for future implementation.

--- lib/views/components/logo.dart
+++ lib/views/components/logo.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class Logo extends StatelessWidget {
-  const Logo({super.key});
+// class Logo extends StatelessWidget {
+//   const Logo({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Image.asset(
-      'assets/images/logo.png',
-      width: 100,
-      height: 100,
-      fit: BoxFit.contain,
-      color: Theme.of(context).colorScheme.primary,
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Image.asset(
+//       'assets/images/logo.png',
+//       width: 100,
+//       height: 100,
+//       fit: BoxFit.contain,
+//       color: Theme.of(context).colorScheme.primary,
+//     );
+//   }
+// }
