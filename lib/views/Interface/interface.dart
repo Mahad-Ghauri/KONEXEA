0337c07ae4acf9ee3024b78a3a5e76bdd6b85c45
Mahad-Ghauri <mahadghauri222@gmail.com>
2025-04-06 17:31:19 +0500

    Refactor InterfacePage: Remove unused imports and eliminate the commented-out elegantRoute method to streamline the code and improve readability.

--- lib/views/Interface/interface.dart
+++ lib/views/Interface/interface.dart
@@ -6,12 +6,10 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:social_swap/controllers/Services/Authentication/authentication_controller.dart';
-import 'package:social_swap/views/Authentication/login.dart';
 import 'package:social_swap/views/Interface/drawer_page.dart';
 import 'package:social_swap/views/Interface/feed_page.dart';
 import 'package:social_swap/views/Interface/news_page.dart';
 import 'package:social_swap/views/Interface/upload_page.dart';
-import 'package:social_swap/views/Interface/about_page.dart';
 
 class InterfacePage extends StatefulWidget {
   static const String id = 'InterfacePage';
@@ -94,24 +92,6 @@ class _InterfacePageState extends State<InterfacePage> {
           });
         },
       ),
-    );
-  }
-
-  PageRouteBuilder _elegantRoute(Widget page, String tag) {
-    return PageRouteBuilder(
-      settings: RouteSettings(name: tag),
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var fadeAnimation = Tween<double>(begin: 0, end: 1).animate(animation);
-        var scaleAnimation = Tween<double>(begin: 0.95, end: 1).animate(
-          CurvedAnimation(parent: animation, curve: Curves.easeOutExpo),
-        );
-        return FadeTransition(
-          opacity: fadeAnimation,
-          child: ScaleTransition(scale: scaleAnimation, child: child),
-        );
-      },
-      transitionDuration: const Duration(milliseconds: 500),
     );
   }
 }
