893739157067f86b951a32b593a3d5301e658b3f
Mahad-Ghauri <mahadghauri222@gmail.com>
2025-04-21 23:50:34 +0500

    Refactor HomePage: Improve text styling and enhance drawer header for better visibility

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -12,7 +12,70 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Text(
           'Welcome to Social Swap!',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold , fontFamily: GoogleFonts.outfit().fontFamily),
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: GoogleFonts.outfit().fontFamily),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Action to perform when the button is pressed
+        },
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (index) {
+          // Handle navigation based on the selected index
+        },
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                // Handle settings tap
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('About'),
+              onTap: () {
+                // Handle about tap
+              },
+            ),
+          ],
         ),
       ),
     );
