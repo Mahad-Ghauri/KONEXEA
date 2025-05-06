aa9ec854c4752382de25a86f9d33aebbc8bd09aa
Mahad-Ghauri <mahadghauri222@gmail.com>
2025-05-06 23:02:07 +0500

    fix: Add missing import for InputControllers in Forgot Password screen

--- lib/views/Interface/Authentication/forgot_password.dart
+++ lib/views/Interface/Authentication/forgot_password.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: deprecated_member_use
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:social_swap/Controllers/input_controllers.dart';
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
